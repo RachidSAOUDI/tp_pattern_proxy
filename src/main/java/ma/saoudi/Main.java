72d1c20ab253eea18b6fcfbe1168d063f1ae9383
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-03-21 17:12:45 +0000

    proxy

--- src/main/java/ma/saoudi/Main.java
+++ src/main/java/ma/saoudi/Main.java
@@ -1,12 +1,16 @@
 package ma.saoudi;
 
 import ma.saoudi.proxy.Context;
+import ma.saoudi.proxy.Proxy;
 import ma.saoudi.proxy.ServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         Context context=new Context();
-        context.setService(new ServiceImpl());
+        // sans proxy
+//        context.setService(new ServiceImpl());
+        // via proxy
+        context.setService(new Proxy());
         context.compute();
     }
 }
--- src/main/java/ma/saoudi/proxy/Proxy.java
+++ src/main/java/ma/saoudi/proxy/Proxy.java
@@ -1,2 +1,12 @@
-package ma.saoudi.proxy;public class Proxy {
+package ma.saoudi.proxy;
+
+public class Proxy implements Service {
+    private ServiceImpl service=new ServiceImpl();
+    @Override
+    public double compute() {
+        System.out.println("Proxy before");
+        double result = service.compute();
+        System.out.println("Proxy after");
+        return result;
+    }
 }
