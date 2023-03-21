a22dc94d9116bce2d25e9c1e009186c1ccf29d52
RachidSAOUDI <rachid.saoudi@usmba.ac.ma>
2023-03-21 16:32:28 +0000

    Logging proxy

--- src/main/java/ma/saoudi/Main.java
+++ src/main/java/ma/saoudi/Main.java
@@ -6,21 +6,6 @@ public class Main {
     public static void main(String[] args) {
         SecurityContext.authenticate("user1","1234","ADMIN");
         Context context=new Context();
-        // sans proxy
-//        context.setService(new ServiceImpl());
-        // via proxy
-//        context.setService(new Proxy());
-        // SECURITY PROXY
-//        context.setService(new SecurityProxy());
-        // Cache Proxy
-//        context.setService(new CacheProxy());
-//        context.compute(3);
-//        context.compute(4);
-//        context.compute(7);
-//        context.compute(3);
-//        context.compute(7);
-//        context.compute(7);
-        // Logging Proxy
         context.setService(new LogginProxy());
         context.compute(6);
     }
--- src/main/java/ma/saoudi/proxy/ServiceImpl.java
+++ src/main/java/ma/saoudi/proxy/ServiceImpl.java
@@ -4,6 +4,11 @@ public class ServiceImpl implements Service {
     @Override
     public double compute(int parameter) {
         double value=53;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return value*parameter;
     }
 }
